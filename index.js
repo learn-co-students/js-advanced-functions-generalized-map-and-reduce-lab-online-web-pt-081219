6f679af1cc94a5d8b2c6fb7957eb5f1051b94e2f
Shaqqour <mshaqqourm@gmail.com>
2020-03-26 18:15:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+
+//MAP
+function map(sourceArray,fun) {
+    let newSource = []
+    sourceArray.forEach(element => newSource.push(fun(element)));
+    return newSource;
+}
+
+function reduce(sourceArray, fun, startingPoint = 0) {
+    sourceArray.forEach(element => startingPoint = fun(element, startingPoint));
+    return startingPoint;
+}
