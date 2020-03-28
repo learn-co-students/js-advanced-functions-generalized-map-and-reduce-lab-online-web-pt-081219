227f03d65a7b02972f601fcce4e4698b85c193ef
Christian Cain <christian22cain@yahoo.com>
2020-03-27 21:16:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(array, fn){
+    let result = [];
+
+    for(const el in array){
+        let value = fn(array[el]);
+        result.push(value)
+    }
+
+    return result;
+}
+
+function reduce(array, fn, starting){
+    let result = (!!starting) ? starting : array[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < array.length; i++) {
+      result = fn(array[i], result)
+    }
+  
+    return result;
+  }
