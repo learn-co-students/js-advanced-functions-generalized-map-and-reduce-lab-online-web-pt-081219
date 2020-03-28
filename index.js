7cdf53c4f681797b4e2ae442a32176dbc229178a
JCGuest <jcguest90@gmail.com>
2020-03-27 23:18:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(arry, block) {
+    let output = arry.map(function(element) { return block(element) })
+    return output
+}
+
+// function reduce(arry, cb, start=0) {
+//     if (typeof(arry[0]) === "number" )
+//     { return arry.reduce(cb, start) }
+// }
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
