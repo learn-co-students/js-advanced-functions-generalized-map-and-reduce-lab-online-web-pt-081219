b884337334ee7f68d80111cb67fb37b9b69ba26e
kierra <51520037+kierrakay@users.noreply.github.com>
2020-03-31 18:16:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your functions here
+// Add your functions here 
+function map(src, cb) {
+    let r = []
+    for (let i =0; i < src.length; i++){
+        let theElement = src [i]
+        r.push(cb(theElement))
+    }
+    return r
+}
+
+// cb means callback
+
+
+
+
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
+
+
+
+
+// function reduce(src, cb, starting){
+//     let r = (!!starting) ? starting : src[0]
+//     // above returns true or false values (left true right false)
+//     let i = (!!starting) ? 0 : 1
+// //   above returns true value present or no true present
+//     for (; i < src.length; i++) {
+//       r = cb(src[i], r)
+//     }
+  
+//     return r;
