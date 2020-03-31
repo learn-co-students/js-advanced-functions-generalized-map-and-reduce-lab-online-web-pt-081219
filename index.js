3be961908c4492e74d9e8d39b94db0af8d631bf5
Jenna Ransom <jenna.ransom@gmail.com>
2020-03-30 18:25:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(array, func) {
+  let newArr = []
+  for (let x in array) {
+    newArr.push(func(array[x]))
+  }
+  return newArr
+};
+
+
+function reduce(array, func, intial) {
+  let result = (!!intial) ? intial : array[0]
+  
+  for (let i = (!!intial) ? 0 : 1; i < array.length; i++) {
+    result = func(array[i], result)
+  }
+  return result
+};
