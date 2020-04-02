cc031bea39e47222e879d1030ee36ef3395387c6
Alec Hall <alecnhall@gmail.com>
2020-04-02 16:06:12 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(array, block){
+    let output = array.map(function(element){ return block(element)});
+    return output;
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+}
